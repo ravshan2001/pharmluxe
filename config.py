6185815ab789cc94315f15273fd63decd64f4aa8
Abdunabiyev Ravshanjon <110659688+ravshan2001@users.noreply.github.com>
2025-08-12 13:53:20 +0500

    config.py

--- config.py
+++ config.py
@@ -1,12 +1,3 @@
-BOT_TOKEN = '8259714661:AAFouer9Sus2yOKVRvAymtY9DNncKYR2QXw'
-ADMIN_CHAT_ID = 634731252
-ONE_C_URL = "https://1c-uz.pharmlux.uz/phl/hs/bot_api/"
-ALLOWED_USERS = {
-634731252, 1281264805
-}
-ONE_C_USER = 'Exchange'
-ONE_C_PASS = "Exchanger"
-
 from aiogram.types import BotCommand
 
 async def set_commands(bot):
